4e0209ab31076ba00ec10b5eb44044bcefa1bf19
Ivan Bokhvalov <bvancha@gmail.com>
2023-01-16 19:08:19 +0100

    end of the day

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,54 @@
 const express = require('express')
+const { listContacts, getContactById, addContact, removeContact, updateContact } = require('../../models/contacts')
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const data = await listContacts()
+  res.json({
+    status: 'success',
+    code: 200,
+    data,
+  });
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const data = await getContactById(contactId)
+  res.json({
+    status: 'success',
+    code: 200,
+    data,
+  });
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const data = await addContact(req.body)
+  res.json({
+    status: 'success',
+    code: 201,
+    data,
+  });
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const data = await removeContact(contactId)
+  res.json({
+    status: 'success',
+    code: 200,
+    data
+  });
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId } = req.params;
+  const data = await updateContact(contactId,req.body)
+  res.json({
+    status: 'success',
+    code: 200,
+    data,
+  });
 })
 
 module.exports = router
